1a60a9f3b377699c569a547428bd2a16b7f8efbe
Sunday Elesi <sundayelesi@gmail.com>
2024-05-21 21:18:29 +0100

    more updates

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 // const sockets = io('https://kserver.okelamedia.com', { autoConnect: true, forceNew: true });
-const sockets = io('/');
+const sockets = io('/#/');
 export default sockets;
