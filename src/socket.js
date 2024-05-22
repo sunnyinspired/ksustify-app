3111892622f0898194adbade748976a33336d76f
Sunday Elesi <sundayelesi@gmail.com>
2024-05-22 08:17:55 +0100

    added homepage

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 // const sockets = io('https://kserver.okelamedia.com', { autoConnect: true, forceNew: true });
-const sockets = io('https://ksustify.netlify.app');
+const sockets = io('/');
 export default sockets;
